bd1c4f220fc1d3f40cc74874f18732c97a905791
Anish Raj Pandey <anishrajpandey@Anishs-MacBook-Air.local>
2024-11-03 01:39:57 -0500

    started dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Navbar from '../_components/Navbar'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <Navbar/>
+    
+    <main className='grid grid-cols-5 w-full h-[70vh] bg-red-300'>
+      
+      <div className='col-span-2'>
+        <Image src={'/Illustration.svg'} width={500 } alt='d' height={500} className='object-contain w-full' />
+      </div>
+    </main>
+    </>
   )
 }
 
