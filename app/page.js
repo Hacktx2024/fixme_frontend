a3741a4ab95388c4e1837c57156a9db714fdcaea
sh1vendra <shivendra@txstate.edu>
2024-11-02 19:23:31 -0500

    Prompt bar updated

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./_components/Navbar";
+import Prompt from "./_components/Prompt";
 
 export default function Home() {
   return (
     <main>
       <Navbar/>
+      <Prompt/>
     </main>
   );
 }
