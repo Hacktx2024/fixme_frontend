3c753428d3c36184c3e202514febc7c455a6301e
Anish Raj Pandey <anishrajpandey@Anishs-MacBook-Air.local>
2024-11-02 23:07:15 -0500

    changes value

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Navbar from "./_components/Navbar";
 import ChatThread from "./_components/ChatThread";
 import Prompt from "./_components/Prompt";
-import FrontPage from "./_components/frontPage";
+import FrontPage from "./_components/FrontPagerontPage";
 
 export default function Home() {
   return (
